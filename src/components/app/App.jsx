c53c9c0315111500ec93d629aedcd9876106f0e4
Sinfinka <inadaradar@gmail.com>
2024-02-18 20:46:01 +0200

    add descr and options

---
+++ src/components/app/App.jsx
@@ -0,0 +1,13 @@
+import Description from "../description/Description";
+import Option from "../options/Options";
+
+function App() {
+  return (
+    <>
+      <Description />
+      <Option />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/options/Options.jsx
@@ -0,0 +1,22 @@
+function Option() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <button>good</button>
+        </li>
+        <li>
+          <button>bad</button>
+        </li>
+        <li>
+          <button>neutral</button>
+        </li>
+        <li>
+          <button>reset</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Option;
